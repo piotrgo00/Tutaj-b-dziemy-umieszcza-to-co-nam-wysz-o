2e3e3348d7584df93d2016cf95ffb2dca9a483c6
pemix09 <pemix09@onet.pl>
2020-05-25 18:54:39 +0200

    Dodanie klas DziałHandlowy, DzialProgramowy, DzialDruku

--- EPRESS/EPRESS/EPRESS/Program.cs
+++ EPRESS/EPRESS/EPRESS/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿/*Wydawnictwo ePress wydaje pozycje: książki różnego rodzaju (sensacyjne, romanse, albumy, ...) oraz 2 rodzaje czasopism: tygodniki oraz miesięczniki. Współpracuje z autorami, których zatrudnia na umowę o pracę lub o dzieło (zajmuje się tym dział programowy wydawnictwa). Drukowaniem zarządza dział druku - odbywa się to w jednej z 3 własnych drukarni, z tym że tylko 1 z nich zapewnia wystarczającą jakość druku dla albumów.
+Minimalny zakres funkcjonalności:
+
+    -zarządzanie autorami (dodawanie, usuwanie, przegląd)
+
+    -zarządzanie umowami i pozycjami (zawieranie umowy o pracę, zawieranie umowy o dzieło na konkretną pozycję, zlecanie w ramach umowy o pracę przygotowania konkretnej pozycji , rozwiązywanie, przegląd)
+
+    -zarządzanie procesem drukowania: dział handlowy przygotowuje zlecenie druku i przesyła je do działu programowego, dział programowy wybiera pasującą drukarnię i przesyła jej zlecenie, drukarnia po wykonaniu druku powiadamia działy druku i handlowy, a ten ostatni wprowadza egzemplarze do swojej oferty
+
+    -bardzo prosty sklep prowadzony przez dział handlowy (funkcjonalności: przyjmowanie zleceń zakupu pozycji zmniejszającej stan magazynowy i pokazywanie katalogu dostępnych pozycji)
+
+    -zapis i odczyt stanu systemu na dysk*/
+using System;
 
 namespace EPRESS
-{
+{  
     class ePress
     {
-        public void dodawanie()
-        {
+        private int wybor;
+        
+        public void dodawanie()//Dodawanie Autora, publikacji, umowy itd. do naszej Bazy danych
+        {                       //różne konstruktory w zależności od typów parametrów?
 
         }
-        public void usuwanie()
-        {
+        public void usuwanie()//usuwanie autorów, publikacji, umówi itd. z naszej bazy danych
+        {                   //trzeba będzie dodać różne konstruktory w zależności od typu parametrów
 
         }
         public void drukowanie()
@@ -24,6 +38,18 @@ namespace EPRESS
         {
 
         }
+    }
+    class DzialDruku
+    {
+
+    }
+    class DzialProgramowy
+    {
+
+    }
+    class DzialHandlowy
+    {
+
     }
     class MainClass
     {

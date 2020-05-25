c3d78ec8b35fa2e25481b5049d81cedbb5043e62
pemix09 <pemix09@onet.pl>
2020-05-25 18:43:11 +0200

    Dodanie funkcji dodawanie, usuwanie, drukowanie, wczytaj, zapisz

--- EPRESS/EPRESS/EPRESS/Program.cs
+++ EPRESS/EPRESS/EPRESS/Program.cs
@@ -2,6 +2,29 @@
 
 namespace EPRESS
 {
+    class ePress
+    {
+        public void dodawanie()
+        {
+
+        }
+        public void usuwanie()
+        {
+
+        }
+        public void drukowanie()
+        {
+
+        }
+        public void wczytaj()
+        {
+
+        }
+        public void zapisz()
+        {
+
+        }
+    }
     class MainClass
     {
         public static void Main(string[] args)
